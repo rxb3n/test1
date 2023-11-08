1896a2ff39cbc192fad7c8faa322c04f855fad46
rxb3n <61215571+rxb3n@users.noreply.github.com>
2023-11-08 01:45:06 +0100

    wth

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,322 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <img src="../src/assets/full.jpeg" alt="">
+
+  <div class="eng">
+    <h1>To English</h1>
+    <div class="input">
+      <input v-model="message" id="toenglish">
+      <button @click=translateAb()>Translate</button>
+      <p>Translation : {{ outputAb }}</p>
+    </div>
+
+  </div>
+
+  <div class="ang">
+    <h1>To Angelish</h1>
+    <div class="input">
+      <input type="text" id="toang">
+      <button @click=translateBa()>Translate</button>
+      <p>Translation : {{ outputBa }}</p>
+    </div>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      outputAb: [],
+      outputBa: [],
+    }
+  },
+
+  methods: {
+    translateAb() {
+      let input = document.getElementById("toenglish").value;
+      this.outputAb = [];
+
+      for (let i = 0; i < input.length; i++) {
+  
+ 
+  if (input[i] == "1") {
+    this.outputAb.push("q");
+  }
+  
+      else if (input[i] == "2") {
+        this.outputAb.push("w");
+  }
+  
+      else if (input[i] == "3") {
+        this.outputAb.push("e");
+  }
+  
+          else if (input[i] == "4") {
+            this.outputAb.push("r");
+  }
+  
+          else if (input[i] == "5") {
+            this.outputAb.push("t");
+  }
+  
+          else if (input[i] == "6") {
+            this.outputAb.push("z");
+  }
+  
+          else if (input[i] == "7") {
+            this.outputAb.push("u");
+  }
+  
+          else if (input[i] == "8") {
+            this.outputAb.push("i");
+  }
+  
+          else if (input[i] == "9") {
+            this.outputAb.push("o");
+  }
+  
+          else if (input[i] == "0") {
+            this.outputAb.push("p");
+  }
+  
+              else if (input[i] == "@") {
+                this.outputAb.push("a");
+  }
+  
+              else if (input[i] == "#") {
+                this.outputAb.push("s");
+  }
+  
+              else if (input[i] == "$") {
+                this.outputAb.push("d");
+  }
+  
+              else if (input[i] == "_") {
+                this.outputAb.push("f");
+  }
+  
+              else if (input[i] == "&") {
+                this.outputAb.push("g");
+  }
+  
+              else if (input[i] == "-") {
+                this.outputAb.push("h");
+  }
+  
+              else if (input[i] == "+") {
+                this.outputAb.push("j");
+  }
+  
+              else if (input[i] == "(") {
+                this.outputAb.push("k");
+  }
+  
+              else if (input[i] == ")") {
+                this.outputAb.push("l");
+  }
+  
+                  else if (input[i] == "*") {
+                    this.outputAb.push("y");
+  }
+  
+                  else if (input[i] == '"') {
+                    this.outputAb.push("x");
+  }
+  
+                  else if (input[i] == "'") {
+                    this.outputAb.push("c");
+  }
+  
+                  else if (input[i] == ":") {
+                    this.outputAb.push("v");
+  }
+  
+                  else if (input[i] == ";") {
+                    this.outputAb.push("b");
+  }
+  
+                  else if (input[i] == "!") {
+                    this.outputAb.push("n");
+  }
+  
+                  else if (input[i] == "?") {
+                    this.outputAb.push("m");
+  }
+  
+                         else if (input[i] == " ") {
+                          this.outputAb.push(" ");
+  }
+
+  
+  
+}
+let outputJ = this.outputAb.join("");
+this.outputAb = outputJ
+console.log(outputJ);
+return outputJ;
+
+
+},
+
+  translateBa() {
+    let input = document.getElementById("toang").value;
+      this.outputBa = [];
+
+      for (let i = 0; i < input.length; i++) {
+        if (input[i] == "q") {
+          this.outputBa.push("1");
+    }
+    
+        else if (input[i] == "w") {
+          this.outputBa.push("2");
+    }
+    
+        else if (input[i] == "e") {
+          this.outputBa.push("3");
+    }
+    
+            else if (input[i] == "r") {
+              this.outputBa.push("4");
+    }
+    
+            else if (input[i] == "t") {
+              this.outputBa.push("5");
+    }
+    
+            else if (input[i] == "z") {
+              this.outputBa.push("6");
+    }
+    
+            else if (input[i] == "u") {
+              this.outputBa.push("7");
+    }
+    
+            else if (input[i] == "i") {
+              this.outputBa.push("8");
+    }
+    
+            else if (input[i] == "o") {
+              this.outputBa.push("9");
+    }
+    
+            else if (input[i] == "p") {
+              this.outputBa.push("0");
+    }
+    
+                else if (input[i] == "a") {
+                  this.outputBa.push("@");
+    }
+    
+                else if (input[i] == "s") {
+                  this.outputBa.push("#");
+    }
+    
+                else if (input[i] == "d") {
+                  this.outputBa.push("$");
+    }
+    
+                else if (input[i] == "f") {
+                  this.outputBa.push("_");
+    }
+    
+                else if (input[i] == "g") {
+                  this.outputBa.push("&");
+    }
+    
+                else if (input[i] == "h") {
+                  this.outputBa.push("-");
+    }
+    
+                else if (input[i] == "j") {
+                  this.outputBa.push("+");
+    }
+    
+                else if (input[i] == "k") {
+                  this.outputBa.push("(");
+    }
+    
+                else if (input[i] == "l") {
+                  this.outputBa.push(")");
+    }
+    
+                    else if (input[i] == "y") {
+                      this.outputBa.push("*");
+    }
+    
+                    else if (input[i] == 'x') {
+                      this.outputBa.push('"');
+    }
+    
+                    else if (input[i] == "c") {
+                      this.outputBa.push("'");
+    }
+    
+                    else if (input[i] == "v") {
+                      this.outputBa.push(":");
+    }
+    
+                    else if (input[i] == "b") {
+                      this.outputBa.push(";");
+    }
+    
+                    else if (input[i] == "n") {
+                      this.outputBa.push("!");
+    }
+    
+                    else if (input[i] == "m") {
+                      this.outputBa.push("?");
+    }
+    
+                       else if (input[i] == " ") {
+                        this.outputBa.push(" ");
+    }
+      }
+      let outputJ = this.outputBa.join("");
+  this.outputBa = outputJ
+  console.log(outputJ);
+  return outputJ;
+
+    }
   }
 }
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+
+body {
+  width: 90%;
+  margin: auto;
+}
+.eng {
+  width: 50%;
+  max-height: 1500px;
+  padding-bottom: 5%;
+  display: flex;
+  flex-direction: column;
+}
+
+.ang {
+  width: 50%;
+  max-height: 1500px;
+  padding-bottom: 5%;
+  display: flex;
+  flex-direction: column;
+}
+
+input {
+  height: 50%;
+  width: 25%;
+}
+
+button {
+  width: 20%;
+  height: 20%;
+}
+
+img {
+  width: 20%;
 }
 </style>
